da904debca1c056e5da8b7e267d80072ae619be7
3c5-o <ffkyyr@gmail.com>
2026-09-13 15:56:49 +0300

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,34 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+const CACHE = 'sharqatna-v6';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+});
+
+self.addEventListener('fetch', e => {
+    if (e.request.method !== 'GET') return;
+    const url = new URL(e.request.url);
+    if (url.origin !== location.origin) return;
+    e.respondWith(
+        caches.match(e.request).then(cached => {
+            const fetched = fetch(e.request).then(res => {
+                if (res.ok) {
+                    const copy = res.clone();
+                    caches.open(CACHE).then(c => c.put(e.request, copy));
+                }
+                return res;
+            }).catch(() => cached);
+            return cached || fetched;
+        })
+    );
+});
